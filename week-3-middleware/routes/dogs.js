9e08323fb54256944497357a4e3a3d76bb28891b
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2026-04-30 20:47:58 -0400

    completed debugging middleware assignment for local dog rescue

--- week-3-middleware/routes/dogs.js
+++ week-3-middleware/routes/dogs.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const dogs = require("../dogData.js");
+const { ValidationError, NotFoundError } = require('../errors');
 
 router.get("/dogs", (req, res) => {
 	res.json(dogs);
 });
 
 router.post("/adopt", (req, res) => {
-    const { name, address, email, dogName } = req.body;
+    const { name, address, email, dogName, } = req.body;
     if (!name || !email || !dogName) {
-        return res.status(400).json({ error: "All fields are required" });
-    }
+        throw new ValidationError("Missing required fields")
 
+    }
+    const dog = dogs.find((d) => d.name === req.body.dogName);
+    if (!dog || dog.status !== "available") {
+  throw new NotFoundError("Dog not found or not available");
+}
     return res.status(201).json({
         message: `Adoption request received. We will contact you at ${email} for further details.`,
     });
