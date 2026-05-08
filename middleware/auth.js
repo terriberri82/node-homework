cd2a6e21f8f991a075605ca6de771e1ca6454008
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2026-05-08 15:05:07 -0400

    updated auth.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const {StatusCodes} = require("http-status-codes");
 const authMiddleware = (req, res, next) => {
     if (global.user_id === null) {
         return res.status(StatusCodes.UNAUTHORIZED)
-                  .json({message:"Unauthorized"})
+                  .json({message:"unauthorized"})
     }
         next();
     
