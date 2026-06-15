daef75cd481f5e381f51a7b050e863060c6de955
TerriAnn Walker <102176320+terriberri82@users.noreply.github.com>
2026-06-15 15:14:21 -0400

    added bulk delete and bulk add controller, routes, and schema

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
-const { create, index, show, update, deleteTask, bulkCreate } = require("../controllers/taskController");
+const { create, index, show, update, deleteTask, bulkCreate, bulkUpdate, bulkDelete } = require("../controllers/taskController");
 const jwtMiddleware = require("../middleware/jwtMiddleware");
 
 router.use(jwtMiddleware);
 router.route("/").post(create);
 router.route("/").get(index);
 router.route("/bulk").post(bulkCreate);
+router.route("/bulk").patch(bulkUpdate);
+router.route("/bulk").delete(bulkDelete)
 router.route("/:id").get(show);
 router.route("/:id").patch(update);
 router.route("/:id").delete(deleteTask);
